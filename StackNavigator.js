a82c68f2821c7a234fb884258019948e09d5617e
Stephen Martinez <122754065+stephenm7777@users.noreply.github.com>
2024-05-03 23:03:14 -0700

    update

--- StackNavigator.js
+++ StackNavigator.js
@@ -7,14 +7,14 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
 const StackNavigator = () => {
     const Stack = createNativeStackNavigator();
-  return (
-    <NavigationContainer>
-        <StackNavigator.Navigator>
-            <StackNavigator.Screen name = "Home" component={LoginScreen}/>
-            <StackNavigator.Screen name = "RegisterScreen" component={RegisterScreen}/>
-        </StackNavigator.Navigator>
-    </NavigationContainer>
-  )
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="Home" component={LoginScreen} options={{ headerShown: false }} />
+                <Stack.Screen name="Register" component={RegisterScreen} options={{ headerShown: false }} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
 }
 
 export default StackNavigator
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,12 +1,26 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { KeyboardAvoidingView, StyleSheet, Text, TextInput, View } from 'react-native'
 import React from 'react'
 
 const LoginScreen = () => {
-  return (
-    <View>
-      <Text>LoginScreen</Text>
-    </View>
-  )
+    return (
+        <View style={{ flex: 1, backgroundColor: "#010C80", padding: 10, alignItems: "center" }}>
+            <KeyboardAvoidingView>
+                <View style={{ marginTop: 100, justifyContent: "center", alignItems: "center" }}>
+                    <Text style={{ color: "#F8FAFC", fontSize: 17, fontWeight: "600" }}>Sign in</Text>
+                    <Text style={{ color: "#F8FAFC", fontSize: 17, fontWeight: "600", marginTop: 15 }}>Sign In to Your Account</Text>
+                </View>
+
+                <View style={{marginTop:50}}>
+                    <View>
+                        <Text style={{ color: "#F8FAFC", fontSize:18, fontWeight:"600"}}>Email</Text>
+                        <TextInput  style ={{borderBottomColor:"#F8FAFC", borderBottomWidth:1, marginVertical:10, width:300}}
+                            placeholderTextColor={"#F8FAFC"} 
+                            placeholder='Enter email address '/>
+                    </View>
+                </View>
+            </KeyboardAvoidingView>
+        </View>
+    )
 }
 
 export default LoginScreen
